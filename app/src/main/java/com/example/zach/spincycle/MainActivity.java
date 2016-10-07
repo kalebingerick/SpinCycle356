583f5152710cc5a22ecd96b250b873fdde1fbeb1
Zach <zsam56@gmail.com>
2016-10-07 14:48:52 -0400

    basic UI menu

--- app/src/main/java/com/example/zach/spincycle/MainActivity.java
+++ app/src/main/java/com/example/zach/spincycle/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.zach.spincycle;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setMenuButtons();
     }
+
+    private void setMenuButtons() {
+        setMenuButton1();
+        setMenuButton2();
+    }
+
+    private void setMenuButton1(){
+        Button testButton = (Button) findViewById(R.id.test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.second_page);
+                setBackButton();
+                playSound();
+            }
+        });
+    }
+
+    private void setMenuButton2(){
+        Button testButton = (Button) findViewById(R.id.test_button2);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.second_page);
+                setBackButton();
+                playSound();
+            }
+        });
+    }
+
+    private void setBackButton(){
+        Button backButton = (Button) findViewById(R.id.back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+                setMenuButtons();
+            }
+        });
+    }
+
+    public void playSound(){
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onBackPressed(){
+        setContentView(R.layout.activity_main);
+        setMenuButtons();
+    }
+
 }
