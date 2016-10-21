4d3f37cdd585ffe566cef273c89ca74b9580404c
jlt8213 <jlt8213@rit.edu>
2016-10-21 10:26:11 -0400

    It was too big

--- app/src/main/java/com/example/zach/spincycle/GPS_Display.java
+++ app/src/main/java/com/example/zach/spincycle/GPS_Display.java
@@ -174,28 +174,14 @@ public class GPS_Display extends AppCompatActivity implements LocationListener {
     }
 
     class Coords{
-        private double lat;
-        private double lon;
-
+        private double lat, lon;
         public Coords(double lat, double lon) {
             this.lat = lat;
             this.lon = lon;
         }
-
-        public double getLat() {
-            return lat;
-        }
-
-        public double getLon() {
-            return lon;
-        }
-
+        public double getLat() {return lat;}
+        public double getLon() {return lon;}
         @Override
-        public String toString() {
-            return "(" + lat +
-                    ", " + lon +
-                    ')';
-        }
+        public String toString() { return "(" + lat + ", " + lon + ')'; }
     }
 }
-
